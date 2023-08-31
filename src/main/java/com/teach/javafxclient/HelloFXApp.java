098115cea969f67f7092cb3d845f213d9a96fe22
陈奎运 <11909414+chen-kuiyun@user.noreply.gitee.com>
2023-08-31 12:56:06 +0000

    src
    Signed-off-by: 陈奎运 <11909414+chen-kuiyun@user.noreply.gitee.com>

---
+++ src/main/java/com/teach/javafxclient/HelloFXApp.java
@@ -0,0 +1,10 @@
+package com.teach.javafxclient;
+
+import javafx.application.Application;
+
+public class HelloFXApp{
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        Application.launch(MainApplication.class);
+    }
+}
